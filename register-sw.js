a1210ccfa5013d6734b2c3b63a15de1068b9dcdc
Nithin <nithin.danday@gmail.com>
2020-12-02 22:34:18 -0800

    ff

--- register-sw.js
+++ register-sw.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js');
+        navigator.serviceWorker.register('./service-worker.js');
     });
 }
